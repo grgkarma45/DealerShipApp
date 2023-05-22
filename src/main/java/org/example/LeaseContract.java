e5bb2f06ac982d43f9d4bfe71c032b293e1047eb
grgkarma45 <grgkarma45@gmail.com>
2023-05-22 15:28:14 -0400

    3pm

--- src/main/java/org/example/LeaseContract.java
+++ src/main/java/org/example/LeaseContract.java
@@ -3,18 +3,41 @@ package org.example;
 public class LeaseContract extends Contract {
     public LeaseContract(String date, String customerName, String customerEmail, Vehicle vehicle) {
         super(date, customerName, customerEmail, vehicle);
-
     }
+
     @Override
     public double getTotalPrice() {
         return 0;
     }
+
     @Override
-    public double getMonthlyPayment(){
-        return 0;
+    public double getMonthlyPayment() {
+        Vehicle vehicleSold = getVehicle(); // Assuming the vehicleSold is the one passed to the constructor
+        double vehiclePrice = vehicleSold.getPrice();
+        double p = vehiclePrice;
+        double r;
+        double n;
+        double t;
+
+        if (vehiclePrice >= 10000) {
+            r = 0.0425;
+            t = 4;
+            n = 12;
+        } else {
+            r = 0.0525;
+            t = 2;
+            n = 12;
+        }
+
+        double rOverN = r / n;
+        double numerator = p * rOverN;
+        double onePlusROverN = 1 + rOverN;
+        double denominator = Math.pow(onePlusROverN, -t * n);
+        return numerator / (1 - denominator);
     }
+
     @Override
-    public String getPersistenceString(){
+    public String getPersistenceString() {
         return null;
     }
 }
--- src/main/java/org/example/SalesContract.java
+++ src/main/java/org/example/SalesContract.java
@@ -5,32 +5,53 @@ public class SalesContract extends Contract {
         super(date, customerName, customerEmail, vehicle);
     }
 
-
     @Override
     public double getTotalPrice() {
         double vehiclePrice = 0;
         double totalPrice = 0;
-        Vehicle vehilceSold = null;
-        vehiclePrice = vehilceSold.getPrice();
-        // this is the processing fee
-        if(vehiclePrice < 10000) {
+        Vehicle vehicleSold = getVehicle(); // Assuming the vehicleSold is the one passed to the constructor
+        vehiclePrice = vehicleSold.getPrice();
+
+        // Processing fee
+        if (vehiclePrice < 10000) {
             totalPrice = vehiclePrice + 295;
+        } else {
+            totalPrice = vehiclePrice + 495;
         }
-        else{
-            totalPrice = vehiclePrice +495;
-        }
-        totalPrice = totalPrice +100;
-        totalPrice = totalPrice + (vehiclePrice *0.05);
+
+        totalPrice = totalPrice + 100;
+        totalPrice = totalPrice + (vehiclePrice * 0.05);
         return totalPrice;
     }
+
     @Override
-    public double getMonthlyPayment(){
-        return 0;
+    public double getMonthlyPayment() {
+        Vehicle vehicleSold = getVehicle(); // Assuming the vehicleSold is the one passed to the constructor
+        double vehiclePrice = vehicleSold.getPrice();
+        double p = vehiclePrice;
+        double r;
+        double n;
+        double t;
+
+        if (vehiclePrice >= 10000) {
+            r = 0.0425;
+            t = 4;
+            n = 12;
+        } else {
+            r = 0.0525;
+            t = 2;
+            n = 12;
+        }
+
+        double rOverN = r / n;
+        double numerator = p * rOverN;
+        double onePlusROverN = 1 + rOverN;
+        double denominator = Math.pow(onePlusROverN, -t * n);
+        return numerator / (1 - denominator);
     }
+
     @Override
-    public String getPersistenceString(){
+    public String getPersistenceString() {
         return null;
     }
-
-
 }
